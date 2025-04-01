40ab9e80a5087dd1bec25e76511174a490ad0338
1Ruslan3 <keshabyn@mail.ru>
2025-04-01 21:48:26 +0300

    Clean coments

--- main.go
+++ main.go
@@ -6,18 +6,18 @@ type Rectangle struct {
 	Width, Height float64
 }
 
-func (r Rectangle) Area() float64 { // Метод по значению
+func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
 }
 
-func (r *Rectangle) Scale(factor float64) { // Метод по указателю
+func (r *Rectangle) Scale(factor float64) {
 	r.Width *= factor
 	r.Height *= factor
 }
 
 func main() {
 	rect := Rectangle{Width: 2, Height: 3}
-	fmt.Println(rect.Area()) // Вывод: 6
+	fmt.Println(rect.Area())
 	rect.Scale(2)
-	fmt.Println(rect.Area()) // Вывод: 12
+	fmt.Println(rect.Area())
 }
