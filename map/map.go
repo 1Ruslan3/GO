311812b33923e2cf9abfbd77e021f5528d9e1325
1Ruslan3 <keshabyn@mail.ru>
2025-04-07 10:13:54 +0300

    map.go

---
+++ map/map.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	ages := map[string]int{
+		"alice":  31,
+		"mark":   29,
+		"ruslan": 21,
+	}
+
+	ages["alice"] = 32
+	ages["bob"] = 12
+
+	for name, age := range ages {
+		fmt.Printf("%s\t%d\n", name, age)
+	}
+}
